c0370157da6b2f858e5b562fee1b0ddbe5e1939c
Pierce <pierce.steward@gmail.com>
2020-11-13 16:41:49 -0700

    stub boot project with JPA Project

---
+++ RaidRecorder/src/main/java/com/skilldistillery/raid/RaidRecorderApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.raid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaidRecorderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaidRecorderApplication.class, args);
+	}
+
+}
---
+++ RaidRecorder/src/test/java/com/skilldistillery/raid/RaidRecorderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.raid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaidRecorderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
