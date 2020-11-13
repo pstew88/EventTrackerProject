5411ec710da4997f4808ccb9c3e3f6e7599cf0bd
Pierce <pierce.steward@gmail.com>
2020-11-13 16:11:07 -0700

    validate hpa/hibernate config

--- JPARaid/src/main/java/com/skilldistillery/raid/entities/Raid.java
+++ JPARaid/src/main/java/com/skilldistillery/raid/entities/Raid.java
@@ -1,6 +1,70 @@
 package com.skilldistillery.raid.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 @Entity
 public class Raid {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	private String name;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Raid(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public Raid() {
+		super();
+	}
+
+	@Override
+	public String toString() {
+		return "Raid [id=" + id + ", name=" + name + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Raid other = (Raid) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
 
 }
--- JPARaid/src/test/java/com/skilldistillery/raid/entities/RaidTest.java
+++ JPARaid/src/test/java/com/skilldistillery/raid/entities/RaidTest.java
@@ -1,6 +1,12 @@
 package com.skilldistillery.raid.entities;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
@@ -10,25 +16,37 @@ import org.junit.jupiter.api.Test;
 
 class RaidTest {
 
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private Raid raid;
+	
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		emf=Persistence.createEntityManagerFactory("RaidPU");
 	}
 
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
+		emf.close();
 	}
 
 	@BeforeEach
 	void setUp() throws Exception {
+	em = emf.createEntityManager();
+	raid = em.find(Raid.class, 1);
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
+	em.close();
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testEntity() {
+		
+		assertNotNull(raid);
+		assertEquals("ShirtlessGorefist", raid.getName());
+		
 	}
 
 }
