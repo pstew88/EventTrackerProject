4a8b50276a2c1dfdb871529b4744219a4889a86e
Pierce Steward <pierce.steward@gmail.com>
2020-11-20 17:36:06 -0700

    added js and index.html

---
+++ RaidRecorder/src/main/resources/static/js/eventTracker.js
@@ -0,0 +1,9 @@
+window.addEventListener('load', function(){
+	console.log('Script loaded');
+	init();
+});
+
+function init(){
+console.log('in init');
+//TODO: set up event handlers for buttons etc.
+}
